a5c6b43e0a9b250bf5e2be9fa875b81e61853464
mediocrity <romamen81@mail.ru>
2022-09-18 22:15:52 +0300

    add client.py

---
+++ client.py
@@ -0,0 +1,43 @@
+import requests
+
+
+def create_user():
+    url = "http://127.0.0.1:8888/users"
+    data = {
+        "display_name": "kop"
+    }
+    resp = requests.post(url, data)
+    created_user = resp.json()
+    print(created_user)
+
+
+def get_user():
+    url = "http://127.0.0.1:8888/users/666"
+
+    resp = requests.get(url)
+    user = resp.json()
+
+    print(user)
+
+
+def get_users():
+    url = "http://127.0.0.1:8888/users"
+
+    resp = requests.get(url)
+    users = resp.json()
+
+    print(users)
+
+
+def delete_user():
+    url = "http://127.0.0.1:8888/users/666"
+
+    resp = requests.delete(url)
+    users = resp.json()
+
+    print(users)
+
+create_user()
+get_user()
+# get_users()
+# delete_user()
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ data = {
 @app.route('/users',methods=["POST"])
 def create_user():
     request_data = request.get_json()
-    print(request_data)
-    id = str(uuid4())
+    # print(request_data)
+    user_id = str(uuid4())
     new_user = {
-        id: {
+        user_id: {
             "display_name": request_data.get("display_name")
         }
     }
@@ -23,14 +23,16 @@ def create_user():
     print(data)
     return {"success": True}
 
-'''
+
 @app.route('/users/<user_id>',methods=["GET"])
-def get_user():
-    return jsonify({
-        "id": id,
-        request
-    })
-'''
+def get_user(user_id):
+    user = data.get(user_id)
+    user_inf = {
+        "id": user_id,
+        "display_name": user.get("display_name")
+    }
+    print(user_inf)
+    return user_inf
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
